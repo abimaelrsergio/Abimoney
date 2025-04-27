0c0e5385ab8c72c80a173a8efff704e50ab3cb32
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-04-27 16:00:26 -0300

    128. Make changes inside Gateway server to accept service names with lower case

--- v2-spring-cloud-gateway/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ v2-spring-cloud-gateway/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,10 @@ package com.eazybytes.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.*;
+import org.springframework.cloud.gateway.route.builder.*;
+import org.springframework.context.annotation.*;
+
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +14,21 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/eazybank/accounts/**")
+						.filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/eazybank/loans/**")
+						.filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/eazybank/cards/**")
+						.filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}"))
+						.uri("lb://CARDS"))
+				.build();
+	}
 }
