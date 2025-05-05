96fd006e15a123a33a107ab29ef59cd36256c049
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-05-05 17:23:17 -0300

    152. Implementing RateLimiter pattern in accounts

--- v2-spring-cloud-circuit-breaker/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ v2-spring-cloud-circuit-breaker/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.controller;
 import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.*;
 import com.eazybytes.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.*;
 import io.github.resilience4j.retry.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -233,11 +234,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @Operation(
