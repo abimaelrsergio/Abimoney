1f8fc2378b64678b7591cb65984b26c4d803a34d
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-05-27 13:55:34 -0300

    208. Develop message microservice using Spring Cloud Functins - Part 2

--- v2-spring-cloud-Functions-RabbitMQ/message/src/main/java/com/eazybytes/message/functions/MessageFunctions.java
+++ v2-spring-cloud-Functions-RabbitMQ/message/src/main/java/com/eazybytes/message/functions/MessageFunctions.java
@@ -1,7 +1,30 @@
 package com.eazybytes.message.functions;
 
-import org.springframework.context.annotation.Configuration;
+import com.eazybytes.message.dto.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.*;
+
+import java.util.function.*;
 
 @Configuration
 public class MessageFunctions {
+
+    private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+
+    @Bean
+    public Function<AccountsMsgDto, AccountsMsgDto> email(){
+        return accountsMsgDto -> {
+            log.info("Sending email with the details: {}", accountsMsgDto.toString());
+            return accountsMsgDto;
+        };
+    }
+
+    @Bean
+    public Function<AccountsMsgDto, Long> sms(){
+        return accountsMsgDto -> {
+            log.info("Sending sms with the details: {}", accountsMsgDto.toString());
+            return accountsMsgDto.accountNumber();
+        };
+    }
 }
