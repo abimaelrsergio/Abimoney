d35b9ca8c64d7627f1824b30c06d7cd402ee9207
Abimael Sergio <abimaelr.sergio@gmail.com>
2025-05-27 07:52:25 -0300

    194. Implement Authorization inside Gateway server using Roles - part 1

--- v2-spring-cloud-keycloak/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ v2-spring-cloud-keycloak/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.eazybytes.gatewayserver.config;
 
 import org.springframework.context.annotation.*;
+import org.springframework.core.convert.converter.*;
 import org.springframework.http.*;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.*;
 import org.springframework.security.config.annotation.web.reactive.*;
 import org.springframework.security.config.web.server.*;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.*;
 import org.springframework.security.web.server.*;
+import reactor.core.publisher.*;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -14,12 +19,19 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                .pathMatchers("/eazybank/accounts/**").authenticated()
-                .pathMatchers("/eazybank/cards/**").authenticated()
-                .pathMatchers("/eazybank/loans/**").authenticated())
+                .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
+                .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
+                .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
         return serverHttpSecurity.build();
     }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
